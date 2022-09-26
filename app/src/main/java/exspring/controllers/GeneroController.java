74307d42da671009262dc04c22bd29d08ae333db
marcov55 <marcovascogit@gmail.com>
2022-09-26 17:11:18 +0000

    exspring-2

--- app/src/main/java/exspring/controllers/GeneroController.java
+++ app/src/main/java/exspring/controllers/GeneroController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import exspring.models.Genero;
 import exspring.repositories.GeneroRepository;
 
 @Controller
@@ -23,4 +26,12 @@ public class GeneroController {
     public String insert(){
         return "insert";
     }
-}
+
+    @RequestMapping(value = "insert", method = RequestMethod.POST)
+    public String saveInsert(@RequestParam("nome") String nome) {
+        Genero genero = new Genero();
+        genero.setNome(nome);
+        this.generosRepo.save(genero);
+        return "redirect:/generos/list";
+    }
+}   
